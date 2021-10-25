e2eb174198ce07e6fcaaf62d31e06364a1a87d29
Matthew Nickson <mnickson@sidingsmedia.com>
2021-10-25 20:02:25 +0100

    Added API for window control
    An API for window control has been added to the preload file.
    
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- electron/preload.ts
+++ electron/preload.ts
@@ -3,7 +3,38 @@
 
 import { contextBridge, ipcRenderer } from "electron";
 
-// TODO: Implement settings in electron.ts
-contextBridge.exposeInMainWorld("settings", {
-    getSettings: () => ipcRenderer.invoke("get-settings"),
+var ctrlChannel: string = "win-ctrl";
+var ipcRegistered = new Set();
+
+/**
+ * Checks if the provided IPC channel has been registered.
+ * Also writes error to console
+ * @param {string} channel - The IPC channel
+ * @returns {boolean} Is the specified IPC channel registered
+ */
+function checkIpc(channel: string): boolean {
+    if (ipcRegistered.has(channel)) {
+        return true;
+    } else {
+        console.error(`IPC Channel ${channel} has not been registered`);
+        return false;
+    }
+}
+
+contextBridge.exposeInMainWorld("window-control", {
+    registerIPC: (channel: string, callback: Function) => {
+        ctrlChannel = channel;
+        ipcRenderer.on(ctrlChannel, (event, arg) => {
+            callback(arg);
+        });
+        ipcRegistered.add(ctrlChannel);
+    },
+    openDevTools: () => ipcRenderer.send(ctrlChannel, "openDevTools"),
+    closeDevTools: () => ipcRenderer.send(ctrlChannel, "closeDevTools"),
+    toggleDevTools: () => ipcRenderer.send(ctrlChannel, "toggleDevTools"),
+    close: () => ipcRenderer.send(ctrlChannel, "close"),
+    minimize: () => ipcRenderer.send(ctrlChannel, "minimize"),
+    maximize: () => ipcRenderer.send(ctrlChannel, "maximize"),
+    unMaximize: () => ipcRenderer.send(ctrlChannel, "unMaximize"),
+    toggleFullScreen: () => ipcRenderer.send(ctrlChannel, "toggleFullScreen"),
 });

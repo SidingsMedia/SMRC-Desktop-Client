0bac1805e21ac5b8ad8f928d3638e9937f267122
Computroniks <mnickson@sidingsmedia.com>
2021-11-21 00:21:17 +0000

    Some formatting and cleanup
    Signed-off-by: Computroniks <mnickson@sidingsmedia.com>

--- main-process/electron/ui.ts
+++ main-process/electron/ui.ts
@@ -2,24 +2,22 @@
 // SPDX-License-Identifier: MIT
 
 import { app, protocol, BrowserWindow } from "electron";
-import { EventEmitter } from "stream";
+import { EventEmitter } from "events";
 import { Window } from "./base/Window";
-
 import * as path from "path";
 import * as url from "url";
 
+export class UI extends EventEmitter {
+    private windows: Array<Window>;
 
-export class UI extends EventEmitter{
-    private windows:Array<Window>
+    constructor() {
+        super();
+        this.windows = [];
 
-    constructor(){
-        super()
-        this.windows = []
-
-        app.whenReady().then(() => {            
-            this.createWindow('index.html')
+        app.whenReady().then(() => {
+            this.createWindow("index.html");
             this.setupLocalFilesNormalizerProxy();
-        
+
             app.on("activate", this.activate);
         });
 
@@ -38,56 +36,65 @@ export class UI extends EventEmitter{
     /** Create a window
      * @param firstFile - The filename with file extension of the HTML
      * file in the parent repository to be loaded.
+     *
+     * @emits window-created
      */
-    createWindow(firstFile:String): void{
+    createWindow(firstFile: String): void {
         // Make sure to get compiled React files from correct place
         const appURL = app.isPackaged
-                ? url.format({
-                      pathname: path.join(__dirname, `../${firstFile}`),
-                      protocol: "file",
-                      slashes: true,
-                  })
-                : `http://localhost:3000/${firstFile}`;
-        let length = this.windows.length
-        this.windows.push(new Window({ url: appURL }, length))
-        let index = length
+            ? `file://${path.join(__dirname, `../${firstFile}`)}`
+            : `http://localhost:3000/${firstFile}`;
+        console.log(appURL);
+        let length = this.windows.length;
+        this.windows.push(new Window({ url: appURL }, length));
+        let index = length;
         // Open dev tools if in dev
         if (!app.isPackaged) {
             this.windows[index].openDevTools();
         }
-        this.emit('window-created')
+        this.emit("window-created", length);
     }
 
     /** Closes a window with the given ID
-     * 
+     *
      * @param windowID - The ID of the window to close
      */
-    closeWindow(windowID:number):void{
-        this.windows[windowID].close()
+    closeWindow(windowID: number): void {
+        this.windows[windowID].close();
     }
-    
+
+    /** Quit
+     *
+     * Makes sure that everything is ready for quit and then quits the
+     * app.
+     *
+     * @emits will-quit
+     */
+    private quit(): void {
+        this.emit("will-quit");
+        app.quit();
+    }
+
     /** Called when the activate event is emmited by app
-     * 
+     *
      * All that this does is check if any windows already exist and if
      * none exist creates a new main window.
      */
-    private activate(): void{
+    private activate(): void {
         // macOS expects that a new window is created if
         // the doc icon is clicked
         if (BrowserWindow.getAllWindows().length === 0) {
-            this.createWindow('index.html')
+            this.createWindow("index.html");
         }
     }
 
-
     /** Called when the windows-all-closed event is emmitted by app
-     * 
+     *
      * If we are on darwin then nothing happens else the app exits.
      */
-    private windowsAllClosed(): void{
+    private windowsAllClosed(): void {
         if (process.platform !== "darwin") {
-            this.emit('quitting')
-            app.quit();
+            this.quit();
         }
     }
 

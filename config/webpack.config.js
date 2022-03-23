7a52d390ca106b39191157882f93f99898f92161
Computroniks <mnickson@sidingsmedia.com>
2022-03-23 20:19:28 +0000

    Fixed reuse
    Signed-off-by: Computroniks <mnickson@sidingsmedia.com>

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022 Sidings Media
+
+// SPDX-License-Identifier: MIT
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const mainConfig = {
